24d16c61ee4d9ebcb73bb8d7f8311b6545ae636d
Ahmet Türk <ahmetturk93@gmail.com>
2017-10-12 19:19:26 +0300

    ADD add_cookie for bot

--- src/steam_cookie.erl
+++ src/steam_cookie.erl
@@ -41,6 +41,21 @@ start_link() ->
 init([]) ->
   {ok, #state{}}.
 
+handle_call({add_cookie, BotName, Name, Value}, _From, State = #state{bots = Bots}) ->
+  Bot = case proplists:is_defined(BotName, Bots) of
+             false ->
+               #bot_state{username = BotName};
+             true ->
+               proplists:get_value(BotName, Bots)
+           end,
+
+  NewBot = case proplists:is_defined(Name,Bot#bot_state.cookies) of
+             false->
+               [proplists:property(Name)
+           end,
+
+  {reply, ok, State#state{cookies = NewCookies}};
+
 handle_call({add_cookie, Name, Value}, _From, State = #state{cookies = Cookies}) ->
   NewCookies = case proplists:is_defined(Name, Cookies) of
                  false -> [proplists:property(Name, Value) | Cookies];
