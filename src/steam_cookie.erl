dc624954ed1cb289facdfa8d273723f6ace66451
Ahmet Türk <ahmetturk93@gmail.com>
2017-10-13 09:34:41 +0300

    ADD get_cookie

--- src/steam_cookie.erl
+++ src/steam_cookie.erl
@@ -43,25 +43,44 @@ init([]) ->
 
 handle_call({add_cookie, BotName, Name, Value}, _From, State = #state{bots = Bots}) ->
   Bot = case proplists:is_defined(BotName, Bots) of
+          false ->
+            #bot_state{username = BotName};
+          true ->
+            proplists:get_value(BotName, Bots)
+        end,
+
+  NewBot = case proplists:is_defined(Name, Bot#bot_state.cookies) of
              false ->
-               #bot_state{username = BotName};
+               Bot#bot_state{cookies = [proplists:property(Name, Value) | Bot#bot_state.cookies]};
              true ->
-               proplists:get_value(BotName, Bots)
+               Bot#bot_state{cookies = lists:keyreplace(Name, 1, Bot#bot_state.cookies, proplists:property(Name, Value))};
            end,
+  NewBots = case proplists:is_defined(BotName, Bots) of
+              false ->
+                [NewBot | Bots];
+              true ->
+                lists:keyreplace(BotName, 1, Bots, NewBot)
+            end,
 
-  NewBot = case proplists:is_defined(Name,Bot#bot_state.cookies) of
-             false->
-               [proplists:property(Name)
-           end,
-
-  {reply, ok, State#state{cookies = NewCookies}};
+  {reply, ok, State#state{bots = NewBots}};
 
 handle_call({add_cookie, Name, Value}, _From, State = #state{cookies = Cookies}) ->
   NewCookies = case proplists:is_defined(Name, Cookies) of
-                 false -> [proplists:property(Name, Value) | Cookies];
-                 true -> lists:keyreplace(Name, 1, Cookies, proplists:property(Name, Value))
+                 false ->
+                   [proplists:property(Name, Value) | Cookies];
+                 true ->
+                   lists:keyreplace(Name, 1, Cookies, proplists:property(Name, Value))
                end,
-  {reply, ok, State#state{cookies = NewCookies}}.
+  {reply, ok, State#state{cookies = NewCookies}};
+
+handle_call({get_cookie, Name}, _From, State = #state{cookies = Cookies}) ->
+  Cookie = case proplists:is_defined(Name, Cookies) of
+             false ->
+               not_found;
+             true ->
+               {ok, proplists:get_value(Name, Cookies)}
+           end,
+  {reply, Cookie, State}.
 
 handle_cast(Request, State) ->
   erlang:error(not_implemented).
