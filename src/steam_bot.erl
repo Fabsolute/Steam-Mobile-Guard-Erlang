5ed89b1395dce3e93ee252447e750fd834e011e5
Ahmet Türk <ahmetturk93@gmail.com>
2017-10-13 18:08:41 +0300

    ADD steam_cookie and headers

--- src/steam_bot.erl
+++ src/steam_bot.erl
@@ -41,29 +41,38 @@ login(Username, Password) ->
 %% gen_server
 
 init([]) ->
-  Cookies = [
-    {"mobileClientVersion", "0213"},
-    {"mobileClient", "android"},
-    {"Steam_Language", "english"}
-  ],
-  Headers = [
-    {"Accept", "text/javascript, text/html, application/xml, text/xml, */*"},
-    {"User-Agent", "Mozilla/5.0 (Linux; U; Android 4.1.1; en-us; Google Nexus 4 - 4.1.1 - API 16 - 768x1280 Build/JRO03S) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"}
-  ],
-  Response = steam_http:mobile_get(?LOGIN_OAUTH, Cookies, Headers ++ [
-    {"X-Requested-With", "com.valvesoftware.android.steam.community"}
-  ]),
-  {ok, #state{cookies = Cookies ++ Response#http_response.cookies, headers = Headers}}.
+  steam_cookie:add_cookie("mobileClientVersion", "0213"),
+  steam_cookie:add_cookie("mobileClient", "android"),
+  steam_cookie:add_cookie("Steam_Language", "english"),
+  steam_cookie:add_header("Accept", "text/javascript, text/html, application/xml, text/xml, */*"),
+  steam_cookie:add_header("User-Agent", "Mozilla/5.0 (Linux; U; Android 4.1.1; en-us; Google Nexus 4 - 4.1.1 - API 16 - 768x1280 Build/JRO03S) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"),
+
+  Response = steam_http:mobile_get(
+    ?LOGIN_OAUTH,
+    steam_cookie:get_cookies(),
+    steam_cookie:get_headers() ++ [
+      {"X-Requested-With", "com.valvesoftware.android.steam.community"}
+    ]
+  ),
+
+  steam_cookie:add_cookies(Response#http_response.cookies),
+  steam_cookie:add_headers(Response#http_response.headers),
+
+  {ok, #state{}}.
 
 handle_call({login, Username, _Password}, _From, State) ->
   RSAPostData = [{<<"username">>, Username}],
-  RSAResponse = steam_http:mobile_post(?LOGIN_GET_RSA_KEY, RSAPostData, State#state.cookies, State#state.headers),
+  RSAResponse = steam_http:mobile_post(
+    ?LOGIN_GET_RSA_KEY,
+    RSAPostData,
+    steam_cookie:get_cookies(Username),
+    steam_cookie:get_headers(Username)
+  ),
   RSABody = RSAResponse#http_response.json_body,
   case maps:get(<<"success">>, RSABody) of
     false -> throw({error, bad_rsa});
     _ -> ok
   end,
-
 
   {reply, RSAResponse, State};
 
--- src/steam_cookie.erl
+++ src/steam_cookie.erl
@@ -2,7 +2,19 @@
 
 -export([
   start_link/0,
-  start/0
+  start/0,
+  add_cookie/2,
+  add_cookie/3,
+  add_header/2,
+  add_header/3,
+  get_cookies/1,
+  get_cookies/0,
+  get_headers/1,
+  get_headers/0,
+  add_cookies/1,
+  add_headers/1,
+  add_headers/2,
+  add_cookies/2
 ]).
 
 -export([
@@ -11,8 +23,7 @@
   handle_cast/2,
   handle_info/2,
   terminate/2,
-  code_change/3
-]).
+  code_change/3]).
 
 -behavior(gen_server).
 
@@ -35,6 +46,57 @@ start() ->
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+add_cookie(Name, Value) ->
+  gen_server:call(?MODULE, {add_cookie, Name, Value}).
+
+add_cookies([]) ->
+  ok;
+
+add_cookies([{Name, Value} | Cookies]) ->
+  add_cookie(Name, Value),
+  add_cookies(Cookies).
+
+add_cookie(BotName, Name, Value) ->
+  gen_server:call(?MODULE, {add_cookie, BotName, Name, Value}).
+
+add_cookies(_BotName, []) ->
+  ok;
+
+add_cookies(BotName, [{Name, Value} | Cookies]) ->
+  add_cookie(BotName, Name, Value),
+  add_cookies(Cookies).
+
+add_header(Name, Value) ->
+  gen_server:call(?MODULE, {add_header, Name, Value}).
+
+add_headers([]) ->
+  ok;
+
+add_headers([{Name, Value} | Headers]) ->
+  add_header(Name, Value),
+  add_headers(Headers).
+
+add_header(BotName, Name, Value) ->
+  gen_server:call(?MODULE, {add_header, BotName, Name, Value}).
+
+add_headers(_BotName, []) ->
+  ok;
+
+add_headers(BotName, [{Name, Value} | Headers]) ->
+  add_header(BotName, Name, Value),
+  add_headers(Headers).
+
+get_cookies(BotName) ->
+  gen_server:call(?MODULE, {get_cookies, BotName}).
+
+get_cookies() ->
+  gen_server:call(?MODULE, get_cookies).
+
+get_headers(BotName) ->
+  gen_server:call(?MODULE, {get_headers, BotName}).
+
+get_headers() ->
+  gen_server:call(?MODULE, get_headers).
 
 %% gen_server
 
@@ -53,7 +115,30 @@ handle_call({add_cookie, BotName, Name, Value}, _From, State = #state{bots = Bot
              false ->
                Bot#bot_state{cookies = [proplists:property(Name, Value) | Bot#bot_state.cookies]};
              true ->
-               Bot#bot_state{cookies = lists:keyreplace(Name, 1, Bot#bot_state.cookies, proplists:property(Name, Value))};
+               Bot#bot_state{cookies = lists:keyreplace(Name, 1, Bot#bot_state.cookies, proplists:property(Name, Value))}
+           end,
+  NewBots = case proplists:is_defined(BotName, Bots) of
+              false ->
+                [NewBot | Bots];
+              true ->
+                lists:keyreplace(BotName, 1, Bots, NewBot)
+            end,
+
+  {reply, ok, State#state{bots = NewBots}};
+
+handle_call({add_header, BotName, Name, Value}, _From, State = #state{bots = Bots}) ->
+  Bot = case proplists:is_defined(BotName, Bots) of
+          false ->
+            #bot_state{username = BotName};
+          true ->
+            proplists:get_value(BotName, Bots)
+        end,
+
+  NewBot = case proplists:is_defined(Name, Bot#bot_state.headers) of
+             false ->
+               Bot#bot_state{headers = [proplists:property(Name, Value) | Bot#bot_state.headers]};
+             true ->
+               Bot#bot_state{headers = lists:keyreplace(Name, 1, Bot#bot_state.headers, proplists:property(Name, Value))}
            end,
   NewBots = case proplists:is_defined(BotName, Bots) of
               false ->
@@ -73,23 +158,49 @@ handle_call({add_cookie, Name, Value}, _From, State = #state{cookies = Cookies})
                end,
   {reply, ok, State#state{cookies = NewCookies}};
 
-handle_call({get_cookie, Name}, _From, State = #state{cookies = Cookies}) ->
-  Cookie = case proplists:is_defined(Name, Cookies) of
+handle_call({add_header, Name, Value}, _From, State = #state{headers = Headers}) ->
+  NewHeaders = case proplists:is_defined(Name, Headers) of
+                 false ->
+                   [proplists:property(Name, Value) | Headers];
+                 true ->
+                   lists:keyreplace(Name, 1, Headers, proplists:property(Name, Value))
+               end,
+  {reply, ok, State#state{headers = NewHeaders}};
+
+handle_call({get_cookies, BotName}, _From, State = #state{bots = Bots, cookies = Cookies}) ->
+  Cookie = case proplists:is_defined(BotName, Bots) of
              false ->
-               not_found;
+               Cookies;
              true ->
-               {ok, proplists:get_value(Name, Cookies)}
+               BotState = proplists:get_value(BotName, Bots),
+               BotState#bot_state.cookies ++ Cookies
            end,
-  {reply, Cookie, State}.
+  {reply, Cookie, State};
 
-handle_cast(Request, State) ->
-  erlang:error(not_implemented).
+handle_call(get_cookies, _From, State = #state{cookies = Cookies}) ->
+  {reply, Cookies, State};
 
-handle_info(Info, State) ->
-  erlang:error(not_implemented).
+handle_call({get_headers, BotName}, _From, State = #state{bots = Bots, headers = Headers}) ->
+  Header = case proplists:is_defined(BotName, Bots) of
+             false ->
+               Headers;
+             true ->
+               BotState = proplists:get_value(BotName, Bots),
+               BotState#bot_state.headers ++ Headers
+           end,
+  {reply, Header, State};
 
-terminate(Reason, State) ->
-  erlang:error(not_implemented).
+handle_call(get_headers, _From, State = #state{headers = Headers}) ->
+  {reply, Headers, State}.
 
-code_change(OldVsn, State, Extra) ->
-  erlang:error(not_implemented).
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, State) ->
+  {noreply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
